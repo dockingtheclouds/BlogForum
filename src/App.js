0138ed30ee009a375ca11920df9404cc47b9f454
juice10-cloud <63415096+juice10-cloud@users.noreply.github.com>
2020-10-22 10:19:38 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Auth, Hub} from 'aws-amplify';
+import {Authenticator, AmplifyTheme} from 'aws-amplify-react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import BlogPage from "./pages/BlogPage";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const UserContext = React.createContext()
+
+class App extends React.Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    this.getUserData();
+    Hub.listen("auth", this, 'onHubCapsule')
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser()
+    user ? this.setState({user}) : this.setState({user: null})
+  }
+
+  onHubCapsule = capsule => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("signed in")
+        this.getUserData()
+        break;
+      case "signUp":
+        console.log("signed up")
+        break;
+      case "signOut":
+        console.log("signed out")
+        this.setState({user: null})
+        break;
+      default:
+        return;
+    }
+  }
+
+  handleSignOut = async () => {
+    try {
+      await Auth.signOut()
+    } catch (err) {
+      console.error('Error signing out user', err)
+    }
+  }
+
+  render() {
+    const {user} = this.state
+
+    return !user ? (
+        <Authenticator theme={theme}/>
+    ) : (
+        <UserContext.Provider value={{user}}>
+          <Router>
+            <>
+              {/*NavBar*/}
+              <Navbar user={user} handleSignOut={this.handleSignOut}/>
+
+              {/*Routes*/}
+              <div className="app-container">
+                <Route exact path={"/"} component={HomePage}/>
+                <Route path={'/blogs/:blogId'}
+                       component={({match}) => (
+                           <BlogPage user={user}
+                                     blogId={match.params.blogId}/>
+                       )}
+                />
+              </div>
+            </>
+          </Router>
+        </UserContext.Provider>)
+  }
 }
+
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: "#ffc0cb"
+  },
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: "var(--amazonOrange)"
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: "5px"
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: "var(--squidInk)"
+  }
+};
 
 export default App;
